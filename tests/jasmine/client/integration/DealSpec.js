ba7fb2c3067df95a375ada7e7601872f5faa5ffc
Jessie Wong <jessie@thisisjessie.com>
2015-06-08 14:53:03 -0700

    Wrote tests for Amy Creates Deal

--- tests/jasmine/client/integration/DealSpec.js
+++ tests/jasmine/client/integration/DealSpec.js
@@ -4,9 +4,18 @@ describe('Deal', function() {
     it('should be created', function(done) {
         Router.go('dealSubmit');
         Meteor.setTimeout(function() {
+        	$('#headline').val('Free help from Charlie Co.');
             $('#company').val('Charlie Co.');
+            $('#details').val('Get professional technical assistance from Charles Dudley of Charlie Co.');
+            $('#street').val('123 Charlie Street');
+            $('#city').val('Charlie Town');
+            $('#state').val('CA');
+            $('#zip').val('94101');
             $('#buttonSubmit').click();
-            expect($('legend').html()).toEqual('Create New Deal');
+            $(window).load(function() {
+            	expect(Router.current().route.path()).toBe('/admin/deals');
+            	expect($('.headline').text()).toContain('Free help from Charlie Co.');
+            });
             done();
         }, 600);
     });
--- tests/jasmine/server/unit/dealModelSpec.js
+++ tests/jasmine/server/unit/dealModelSpec.js
@@ -1,27 +1,21 @@
 //A suite of tests related to the Deals collection.. put related tests into in this block
 "use strict";
 describe("Deal", function() {
-	it("should be created with headline, company, and details", function() {
-		spyOn(Deals, "insert").and.callFake(function(doc, callback) {
-			// Simulate async return of id = "1"
-			callback(null, "1");
-		});
 
-		var deal = new Deal(null, "Sample Headline", "Sample Company", "Sample Details", {street: '123 Main Street', city: 'San Francisco', state: 'CA', zip: '94101'});
+	var deal;
+
+	beforeEach(function() {
+		deal = new Deal(null, "Sample Headline", "Sample Company", "Sample Details", {street: '123 Main Street', city: 'San Francisco', state: 'CA', zip: '94101'});
+	});
+
+	it("should be created with headline, company, and details", function() {
+
+		Meteor.call('saveDeal', deal, function (error, response) {
+			Session.set('saveDeal', response);
+		});
 
 		expect(deal.headline).toBe("Sample Headline");
 		expect(deal.company).toBe("Sample Company");
 		expect(deal.details).toBe("Sample Details");
-
-		deal.save();
-
-		expect(deal._id).toEqual("1");
-		expect(Deals.insert).toHaveBeenCalledWith( {
-			headline: "Sample Headline",
-			company: "Sample Company",
-			details: "Sample Details"
-		},
-			jasmine.any(Function)
-		);
 	});
 });
