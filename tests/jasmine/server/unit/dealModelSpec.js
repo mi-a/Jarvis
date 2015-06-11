6550d17c5abf8ab843fc2d29b8ab523a08df5219
Jessie Wong <jessie@thisisjessie.com>
2015-06-11 13:20:31 -0700

    Added serve unit test for updating existing deals

--- tests/jasmine/server/unit/dealModelSpec.js
+++ tests/jasmine/server/unit/dealModelSpec.js
@@ -5,7 +5,12 @@ describe("Deal", function() {
 	var deal;
 
 	beforeEach(function() {
+		MeteorStubs.install();
 		deal = new Deal(null, "Sample Headline", "Sample Company", "Sample Details", {street: '123 Main Street', city: 'San Francisco', state: 'CA', zip: '94101'});
+	});
+
+	afterEach(function() {
+		MeteorStubs.uninstall();
 	});
 
 	it("should be created with headline, company, and details", function() {
@@ -17,5 +22,21 @@ describe("Deal", function() {
 		expect(deal.headline).toBe("Sample Headline");
 		expect(deal.company).toBe("Sample Company");
 		expect(deal.details).toBe("Sample Details");
+	});
+
+	it("should update an existing deal if deal already exists", function() {
+		Meteor.call('saveDeal', deal, function (error, response) {
+			Session.set('saveDeal', response);
+		});
+
+		expect(deal.headline).toBe("Sample Headline");
+		deal.headline = "New Headline";
+
+		Meteor.call('saveDeal', deal, function (error, response) {
+			Session.set('saveDeal', response);
+		});
+
+		expect(deal.headline).toBe("New Headline");
+		expect(deal.company).toBe("Sample Company");
 	});
 });
